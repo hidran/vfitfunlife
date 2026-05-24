e0ba8a8ef2322f8a399e315fcaee6466c591bfe2
Hidran Arias <hidran.arias@spryker.com>
2026-05-24 17:42:31 +0200

    feat(hooks): useVenue/useVenues/useVenueServices/useVenueCourses

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 // Export all custom hooks
 export { useCapacitor, useKeyboard, useAppState } from './useCapacitor';
+export { useVenue, useVenues, useVenueServices, useVenueCourses } from './useVenues';
